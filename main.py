e97bfdf918c0b06522dd1e9505ababbae2ad8e51
Slavko Konov <sadjazzman@gmail.com>
2024-06-18 18:36:18 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+import os, sys
+sys.path.append('../common')
+sys.path.append(os.path.abspath('/Workspace/Scripts/CDP_EMEA/dp-n01-cdp-emea-consumer-behavior-reporting/src/python/databricks/common'))        
+import cbrep_mappings_new as mapping
