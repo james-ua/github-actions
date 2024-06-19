5589e915469ff13f12f9e37ad6718bee43fe5754
james-ua <sadjazzman@gmail.com>
2024-06-19 08:14:06 +0000

    GH Action Files added Wed Jun 19 08:14:06 UTC 2024

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ sys.path.append('../common')
 sys.path.append(os.path.abspath('/Workspace/Scripts/CDP_EMEA/dp-n01-cdp-emea-consumer-behavior-reporting/src/python/databricks/common'))        
 import cbrep_mappings_new as mapping
 
-a = mapping.utils('hello')
+a = mapping.utils('world')
 d = mapping.utils('Hello')
 print(a,d)
