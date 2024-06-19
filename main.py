4b2e13aec4f995ce6fcaedfa189339e10178f476
Slavko Konov <sadjazzman@gmail.com>
2024-06-19 10:06:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,4 @@ import os, sys
 sys.path.append('../common')
 sys.path.append(os.path.abspath('/Workspace/Scripts/CDP_EMEA/dp-n01-cdp-emea-consumer-behavior-reporting/src/python/databricks/common'))        
 import cbrep_mappings_new as mapping
-a = 'hello, world!'
-b = 'hello'
-c = 'Hello'
-d = 'HELLO'
+
